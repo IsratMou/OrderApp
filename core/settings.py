7df00dd3e04ca41db0566227012892cb7fa106e4
IsratMou <moutherobogirl@gmail.com>
2025-12-06 22:40:33 +0600

    Update settings for deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l5ay($@5^w4o=%@fu)o8zjv(yhql%q&=^ugac+_a4-1ukyb6$m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['isratmoubusiness96.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -143,4 +143,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
